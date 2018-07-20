bdac4e67ec4eaec858192da24699bd6a9be9f8d6
Wilson <wilson.han@doctors-express.com>
2018-07-20 15:54:46 +0800

    示例代码添加

--- app/src/main/java/com/example/wilsonhan/happydevelop/utils/CommonUtils.java
+++ app/src/main/java/com/example/wilsonhan/happydevelop/utils/CommonUtils.java
@@ -1,11 +1,17 @@
 package com.example.wilsonhan.happydevelop.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.util.Log;
 
 import com.example.wilsonhan.happydevelop.app.AppApplication;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,6 +59,48 @@ public class CommonUtils {
             e.printStackTrace();
         }
         return versionName;
+    }
+
+    /**
+     * 播放声音
+     *
+     * @param activity
+     * @return
+     */
+    public static MediaPlayer OpeanAudio(Activity activity, AssetFileDescriptor file) {
+        android.media.AudioManager audioService = (android.media.AudioManager) activity
+                .getSystemService(Context.AUDIO_SERVICE);
+        if (audioService.getRingerMode() != android.media.AudioManager.RINGER_MODE_NORMAL) {
+            shouldPlayBeep = false;
+        }
+
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        // 注册事件。当播放完毕一次后，重新指向流文件的开头，以准备下次播放。
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer player) {
+                player.seekTo(0);
+            }
+        });
+
+//        AssetFileDescriptor file = activity.getResources().openRawResourceFd(R.raw.video);
+
+        try {
+            mediaPlayer.setDataSource(file.getFileDescriptor(),
+                    file.getStartOffset(), file.getLength());
+            file.close();
+            mediaPlayer.setVolume(0.5f, 0.5f);
+            mediaPlayer.prepare();
+        } catch (IOException ioe) {
+            Log.w(TAG, ioe);
+            mediaPlayer = null;
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+        return mediaPlayer;
     }
 
 
